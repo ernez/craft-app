2ec1c419b2f659aa14457d0514f01cb3149bb2bb
Ernez Catovic <--help>
2021-01-10 20:13:08 +0100

    project init finished

--- vue-crafts/src/main.js
+++ vue-crafts/src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import VueFormGenerator from 'vue-form-generator'
+// import VueFormGenerator from 'vue-form-generator'
 import App from './App'
 import TelInput from './components/tel-input.vue'
 import { router } from './router';
@@ -10,6 +10,7 @@ import VeeValidate from 'vee-validate';
 import Vuex from 'vuex';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 import {
   faHome,
   faUser,
@@ -17,6 +18,8 @@ import {
   faSignInAlt,
   faSignOutAlt
 } from '@fortawesome/free-solid-svg-icons';
+
+import vuetify from './plugins/vuetify';
 
 library.add(faHome, faUser, faUserPlus, faSignInAlt, faSignOutAlt);
 
@@ -28,34 +31,28 @@ Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.use(Vuex);
 
 Vue.component('field-tel-input', TelInput)
-Vue.use(VueFormGenerator, {
-  validators: {
-    strongPassword: (value) => {
-      let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{5,}$/
-
-      if (!regex.test(value)) {
-        return [ 'Password should be a minimum five characters, at least one uppercase letter, ' +
-        'one lowercase letter and one number']
-      } else {
-        return []
-      }
-    }
-  }
-})
+// Vue.use(VueFormGenerator, {
+//   validators: {
+//     strongPassword: (value) => {
+//       let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{5,}$/
+//
+//       if (!regex.test(value)) {
+//         return [ 'Password should be a minimum five characters, at least one uppercase letter, ' +
+//         'one lowercase letter and one number']
+//       } else {
+//         return []
+//       }
+//     }
+//   }
+// })
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-// new Vue({
-//   el: '#app',
-//   router,
-//   components: { App },
-//   template: '<App/>'
-// })
 
 /* eslint-disable no-new */
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app');
---
+++ vue-crafts/src/views/user/userVew.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+
+  <section class="user-vew">
+    <h1>user-vew Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'user-vew',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="scss">
+  .user-vew {
+
+  }
+</style>
